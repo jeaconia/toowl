9908e49747676a85b5d60ea5eabfe9f407939f07
jeaconia <tjeaconia8@gmail.com>
2023-12-11 01:01:36 +0800

    jd satu

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VASTlaundry</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+
+</br> </br> </br> </br> </br> </br>
+<div class="container mx-auto p-2 text-center" style="width: 800px;">
+<button type="button" class="btn btn-primary btn-lg" href="homepage.php">VAST LAUNDRY HOMEPAGE</button></br></br>
+<button type="button" class="btn btn-primary btn-lg" href="login-admin.php">LOGIN</button>
+</div> </br> </br>
+
+<footer class="bd-footer py-4 py-md-5 mt-5 bg-primary-subtle">
+  <div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary text-center">
+      <p1>All Rights Reserved ©VASTlaundry 2023</p1></br></br></br>
+      <div class="row align-items-center">
+        <a class="col fs-6" href="https://youtu.be/dQw4w9WgXcQ?si=pX8mEhsOMYARGLf3">
+          Instagram
+        </a>
+        <a class="col fs-6" href="https://youtu.be/BbeeuzU5Qc8?si=crrkoGANXCXw-Xrd">
+          Whatsapp
+        </a>
+        <a class="col fs-6" href="https://youtu.be/n4kluqkCymw?si=DWRcIm0MtOOhOiNp">
+          Line
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  </body>
+</html>
