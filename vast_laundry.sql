5d5db6622c021158bab5537a6553b827903e0db5
alexaprmth <paramitha.2305551096@student.unud.ac.id>
2023-12-17 06:55:44 +0800

    buat ulang

---
+++ vast_laundry.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2023 at 11:52 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vast_laundry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pelanggan`
+--
+
+CREATE TABLE `tb_pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama_pelanggan` varchar(100) NOT NULL,
+  `hp_pelanggan` varchar(15) NOT NULL,
+  `jenis_layanan` enum('Reguler','Express','','') NOT NULL,
+  `jumlah_pakaian` int(11) NOT NULL,
+  `berat_total` int(11) NOT NULL,
+  `harga_total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_pelanggan`
+--
+
+INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `hp_pelanggan`, `jenis_layanan`, `jumlah_pakaian`, `berat_total`, `harga_total`) VALUES
+(3, 'Karin', '081546735547', 'Express', 13, 2, 15000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_register`
+--
+
+CREATE TABLE `tb_register` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_register`
+--
+
+INSERT INTO `tb_register` (`id_admin`, `username`, `password`) VALUES
+(1, 'aaa', '$2y$10$RejMvwVpjCsuGEQ.zmmfduwLnq6o1uzF8qEQxtRAH.pAPQ58Z74Om'),
+(2, 'alexa', '$2y$10$AFRm0/T.vk4RCEYSwYf6Iu66vB4gsd1eAN.KKqwBvmPfg5sYdUm4K'),
+(3, 'karin', '$2y$10$TUzAA5bUmzn.kdCAwSt2FeiQ9.jBj0D6IZKYAo9za3GNKIaatMpL2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `tb_register`
+--
+ALTER TABLE `tb_register`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_register`
+--
+ALTER TABLE `tb_register`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
