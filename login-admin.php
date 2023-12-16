86c06554e395fb6cd393773853d5b2fcae654b5d
alexaprmth <paramitha.2305551096@student.unud.ac.id>
2023-12-17 06:51:04 +0800

    buat ulang

---
+++ login-admin.php
@@ -0,0 +1,58 @@
+<?php
+require 'functions.php';
+
+$conn = mysqli_connect("localhost", "tool", "12345678", "vast_laundry");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT username, password FROM tb_register WHERE username = '$username'");
+
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        
+        if (password_verify($password, $row["password"])) {
+            header("Location: database.php");
+            exit;
+        }
+    }
+
+    $error = true;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+
+    <body>
+        <h1>Login</h1>
+
+        <?php if (isset($error)) : ?>
+            <p style="color: red; font-style: italic;">Username atau password salah!</p>
+        <?php endif; ?>
+
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <label for="username">Username :</label>
+                    <input type="text" name="username" id="username">
+                </li>
+                <li>
+                    <label for="password">Password :</label>
+                    <input type="password" name="password" id="password">
+                </li>
+                <li>
+                    <button type="submit" name="login">Login</button>
+                </li>
+            </ul>
+        </form>
+    </body>
+</html>
