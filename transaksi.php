7f3cd8716ee8e0025fcd7c87ae8a3610ab9e53b3
jeaconia <tjeaconia8@gmail.com>
2023-12-18 08:27:11 +0800

    ngedit lagi

--- transaksi.php
+++ transaksi.php
@@ -1,18 +1,15 @@
-<?php
-    include 'koneksi.php';
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Transaksi VASTlaundry</title>
+    <title>VASTlaundry</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
 
-<nav class="navbar bg-primary-subtle fixed-top">
+
+  <nav class="navbar bg-primary-subtle fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
       <p class="fs-3">VASTlaundry</p>
@@ -28,10 +25,10 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link" href="homepage.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="homepage.php">Home</a>
             <a class="nav-link" href="syaratnketentuan.php">Syarat & Ketentuan</a>
             <a class="nav-link" href="lokasi.php">Lokasi</a>
-            <a class="nav-link active" href="transaksi.php"aria-current="Informasi Transaksi">Info Transaksi</a>
+            <a class="nav-link" href="transaksi.php">Info Transaksi</a>
           </li>
         </ul>
         <form class="d-flex mt-3" role="search">
@@ -43,50 +40,31 @@
   </div>
   </nav>
 
-</br></br></br></br></br>
+  </br></br></br></br></br></br>
+    <div class="container mx-auto p-2 text-center" style="width: 800px;">
+      <a href="transaksi-kiloan.php" class="btn btn-primary btn-lg">Transaksi Kiloan</a></br></br>
+      <a href="transaksi-satuan.php" class="btn btn-primary btn-lg">Transaksi Satuan</a></br></br>
+      <a href="transaksi-sepatu.php" class="btn btn-primary btn-lg">Transaksi Sepatu</a>
+    </div> </br> </br>
 
-<div class="container mx-auto p-2 text-center" style="width: 800px;">
-  <form class="d-flex mt-3" role="search">
-  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-  <button class="btn btn-outline-success" type="submit">Search</button>
-  </form>
-</div>
+<footer class="bd-footer py-4 py-md-5 mt-5 bg-primary-subtle">
+      <div class="container py-4 py-md-5 px-4 px-md-3 text-body-secondary text-center">
+        <a href="homepage.php">All Rights Reserved ©VASTlaundry 2023</a></br></br></br>
+        <div class="row align-items-center">
+          <a class="col fs-6" href="https://youtu.be/dQw4w9WgXcQ?si=pX8mEhsOMYARGLf3">
+            Instagram
+          </a>
+          <a class="col fs-6" href="https://youtu.be/BbeeuzU5Qc8?si=crrkoGANXCXw-Xrd">
+            Whatsapp
+          </a>
+          <a class="col fs-6" href="https://youtu.be/n4kluqkCymw?si=DWRcIm0MtOOhOiNp">
+            Line
+          </a>
+        </div>
+      </div>
+    </footer>
 
-</br></br>
-<div class="container mx-auto p-2" style="width: 700px;">
-  <?php
-    $sql="SELECT * FROM tb_pelanggan;";
-    $result=mysqli_query($koneksi, $sql);
-    if($result) {
-        while($row=mysqli_fetch_assoc($result)) {
-            $id_pelanggan = $row['id_pelanggan'];
-            $nama_pelanggan = $row['nama_pelanggan'];
-            $hp_pelanggan = $row['hp_pelanggan'];
-            $jenis_layanan = $row['jenis_layanan'];
-            $jumlah_pakaian = $row['jumlah_pakaian'];
-            $berat_total = $row['berat_total'];
-            $harga_total = $row['harga_total'];
-            echo '<div class="card">
-                    <div class="card-header">
-                      ID Pemesanan: ' . $id_pelanggan . '<br>
-                      Nama Pelanggan: ' . $nama_pelanggan .'
-                    </div>
-                    <div class="card-body">
-                      <p class="card-text">
-                        No. HP Pelanggan: ' . $hp_pelanggan . '<br>
-                        Jenis Layanan: ' . $jenis_layanan . '<br>
-                        Jumlah Pakaian: ' . $jumlah_pakaian . '<br>
-                        Berat Total: ' . $berat_total . '<br>
-                        Harga Total: ' . $harga_total . '</p>
-                    </div>
-                  </div>
-                  <br>';
-        }
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-    }
-  ?>
-</div>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
